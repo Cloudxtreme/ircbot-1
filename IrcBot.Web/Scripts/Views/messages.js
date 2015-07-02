9c773a8da66cc633892ff85a54293140ce74a998
Adam Stirtan <adamstirtan@gmail.com>
2015-07-02 11:09:31 -0400

    Adding third party libs

--- IrcBot.Web/Scripts/Views/messages.js
+++ IrcBot.Web/Scripts/Views/messages.js
@@ -1,34 +1,40 @@
-﻿require(['jquery', 'bootstrap', 'moment'], function ($, b, moment) {
+﻿$(document).ready(function () {
+    "use strict";
 
-    $(function () {
+    Core.init();
+});
 
-        var today = moment().format('YYYY-MM-DD'),
-            yesterday = moment().add(-1, 'days').format('YYYY-MM-DD');
+//require(['jquery', 'bootstrap', 'moment'], function ($, b, moment) {
 
-        $('#inputFrom').val(yesterday);
-        $('#inputTo').val(today);
+//    $(function () {
 
-        $('#showMessages').click(function (e) {
-            e.preventDefault();
+//        var today = moment().format('YYYY-MM-DD'),
+//            yesterday = moment().add(-1, 'days').format('YYYY-MM-DD');
 
-            var from = $('#inputFrom').val(),
-                to = $('#inputTo').val(),
-                table = $('.table tbody'),
-                createRow = function (row) {
-                    return '<tr><td>' + row.nick + '</td><td>' + '<a href="/messages/' + row.id + '">' + row.content + '</a></td><td nowrap>' + moment(row.created).format('MMMM Do YYYY, h:mm:ss a') + '</td></tr>';
-                };
+//        $('#inputFrom').val(yesterday);
+//        $('#inputTo').val(today);
 
-            $.ajax({
-                url: '/api/messages?from=' + from + '&to=' + to,
-                success: function (result) {
-                    $.each(result, function (index, value) {
-                        table.append(createRow(value));
-                    });
-                },
-                dataType: "json"
-            });
-        });
+//        $('#showMessages').click(function (e) {
+//            e.preventDefault();
+
+//            var from = $('#inputFrom').val(),
+//                to = $('#inputTo').val(),
+//                table = $('.table tbody'),
+//                createRow = function (row) {
+//                    return '<tr><td>' + row.nick + '</td><td>' + '<a href="/messages/' + row.id + '">' + row.content + '</a></td><td nowrap>' + moment(row.created).format('MMMM Do YYYY, h:mm:ss a') + '</td></tr>';
+//                };
+
+//            $.ajax({
+//                url: '/api/messages?from=' + from + '&to=' + to,
+//                success: function (result) {
+//                    $.each(result, function (index, value) {
+//                        table.append(createRow(value));
+//                    });
+//                },
+//                dataType: "json"
+//            });
+//        });
 
         
-    });
-});
+//    });
+//});

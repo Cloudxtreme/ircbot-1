33e9c2a87d2bfccd193ae543120c41a6cd8d84b2
Adam Stirtan <adamstirtan@gmail.com>
2015-07-13 12:59:20 -0400

    Made scream trigger funnier

--- IrcBot.Client/Triggers/IDonScreamTrigger.cs
+++ IrcBot.Client/Triggers/IDonScreamTrigger.cs
@@ -1,4 +1,7 @@
-﻿using Meebey.SmartIrc4net;
+﻿using System;
+using System.Text;
+
+using Meebey.SmartIrc4net;
 
 namespace IrcBot.Client.Triggers
 {
@@ -6,7 +9,37 @@ namespace IrcBot.Client.Triggers
     {
         public void Execute(IrcClient client, IrcEventArgs eventArgs, string[] triggerArgs)
         {
-            client.SendMessage(SendType.Message, eventArgs.Data.Channel, "BWWAAAAAAUUUUUGGGGGHH");
+            var random = new Random(DateTime.Now.Millisecond);
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("B");
+
+            for (int i = 0, n = random.Next(1, 4); i < n; i++)
+            {
+                stringBuilder.Append("W");
+            }
+
+            for (int i = 0, n = random.Next(1, 8); i < n; i++)
+            {
+                stringBuilder.Append("A");
+            }
+
+            for (int i = 0, n = random.Next(1, 8); i < n; i++)
+            {
+                stringBuilder.Append("U");
+            }
+
+            for (int i = 0, n = random.Next(1, 8); i < n; i++)
+            {
+                stringBuilder.Append("G");
+            }
+
+            for (int i = 0, n = random.Next(1, 4); i < n; i++)
+            {
+                stringBuilder.Append("H");
+            }
+
+            client.SendMessage(SendType.Message, eventArgs.Data.Channel, stringBuilder.ToString());
         }
     }
 }

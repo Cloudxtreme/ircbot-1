c68d3568ea145e6615000fb3a7cab227a0f89303
Adam Stirtan <adamstirtan@gmail.com>
2015-07-24 14:38:10 -0400

    WIP of !gnews trigger

---
+++ IrcBot.Client/Triggers/GoogleNewsSearchTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Meebey.SmartIrc4net;
+
+using IrcBot.Client.Triggers.Contracts;
+
+namespace IrcBot.Client.Triggers
+{
+    public class GoogleNewsSearchTrigger : IGoogleNewsSearchTrigger
+    {
+        public void Execute(IrcClient client, IrcEventArgs eventArgs, string[] triggerArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
